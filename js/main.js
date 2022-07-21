cc71954499b8da41f764ca67a50e034aceaf7d1d
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-21 15:04:47 +0200

    project complete

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Benvenuto in Sardegna!',
+      image: 'https://www.sardegna.info/wp-content/uploads/sites/5/torre-la-pelosa.jpg'
+    }
+  })
